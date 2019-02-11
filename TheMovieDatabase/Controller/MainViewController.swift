6dc3f0ca943ba2a3b6a0e64f3bacc1ba5b604b0b
Marcelo Oscar José <marcelo@Marcelos-MacBook-Air.local>
2019-02-11 05:49:01 -0300

    Networking and Models

--- TheMovieDatabase/Controller/MainViewController.swift
+++ TheMovieDatabase/Controller/MainViewController.swift
@@ -7,28 +7,45 @@
 //
 
 import UIKit
-import PureLayout
+import PXStickyHeaderCollectionView
 
 class MainViewController: UIViewController {
 
-    let containerView: UIView = {
-        let containerView = UIView.newAutoLayout()
+    // Collection view ids
+    let KCellId = "collectionCell"
+    let KCellClass = "CollectionViewCell"
+
+    // UI Vars
+    var containerView: PXStickyHeaderCollectionView!
+
+    // Data vars
+    var moviesData: [MovieData] = []
+
+    /*let containerView: UIView = {
+        let containerView = UIView()
         return containerView
-    }()
+    }()*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.translatesAutoresizingMaskIntoConstraints = false
 
-        // Setup UI
-        setupControls()
-        setupConstraints()
-
         getMovies()
     }
 
     func setupControls() {
-        
+        containerView = PXStickyHeaderCollectionView(initHeaderHeight: 200, minHeaderHeight: 50, headerView: UIView())
+        self.view.addSubview(containerView)
+
+        let margins = view.layoutMarginsGuide
+        containerView.topAnchor.constraint(equalTo: margins.topAnchor, constant: 0).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: 0).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 0).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: 0).isActive = true
+
+        containerView.collectionView.register(UINib(nibName: KCellClass, bundle: Bundle(for: MainViewController.self)), forCellWithReuseIdentifier: KCellId)
+        containerView.delegate = self
+        containerView.dataSource = self
     }
 
     func setupConstraints() {
@@ -38,20 +55,21 @@ class MainViewController: UIViewController {
     func getMovies() {
         let movieModel = MovieModel()
         movieModel.getUpcommingMovies(responseHandler: { (movies) in
-            print(movies)
+            self.setupControls()
+            self.setupConstraints()
         }) { (error) in
             print(error!)
         }
     }
 }
 
-/*extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     public func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
 
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.cellModel.count
+        return self.moviesData.count
     }
 
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -59,8 +77,11 @@ class MainViewController: UIViewController {
     }
 
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KCellId, for: indexPath) as! CollectionViewCell
-        cell.configureCell(image: UIImage(named: cellModel[indexPath.item].image), title: cellModel[indexPath.item].title)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KCellId, for: indexPath) as! MovieCollectionViewCell
+
+        let movieData = self.moviesData[indexPath.item]
+        cell.updateCellData(title: movieData.title, releaseDate: movieData.releaseDate, overview: movieData.overview)
+
         return cell
     }
 
@@ -79,4 +100,4 @@ class MainViewController: UIViewController {
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 8
     }
-}*/
+}

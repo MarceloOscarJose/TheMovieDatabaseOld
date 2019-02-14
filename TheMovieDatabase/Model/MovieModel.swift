a13fd4e8e700db66d790ee801c601c05fa0884e2
Marcelo Oscar José <marcelo@Marcelos-MacBook-Air.local>
2019-02-14 15:05:33 -0300

    App images

--- TheMovieDatabase/Model/MovieModel.swift
+++ TheMovieDatabase/Model/MovieModel.swift
@@ -14,25 +14,48 @@ class MovieModel: NSObject {
     var currentPage: Int = 1
 
     func getUpcommingMovies(nextPage: Bool, responseHandler: @escaping (_ response: [MovieData]) -> Void, errorHandler: @escaping (_ error: Error?) -> Void) {
-        let discoverService = DiscoverService()
-        var movies: [MovieData] = []
-
         if nextPage && maxPages > self.currentPage {
             self.currentPage += 1
             print(self.currentPage)
         }
 
         if maxPages > self.currentPage {
-            discoverService.getUpcommingMovies(page: self.currentPage, responseHandler: { (movieResult) in
-                self.maxPages = movieResult.totalPages
-                for movie in movieResult.results {
-                    movies.append(MovieData(movie: movie))
-                }
+            fetchUpcommingMoviews(responseHandler: responseHandler, errorHandler: errorHandler)
+        }
+    }
 
-                responseHandler(movies)
-            }) { (error) in
-                errorHandler(error)
+    private func fetchUpcommingMoviews(responseHandler: @escaping (_ response: [MovieData]) -> Void, errorHandler: @escaping (_ error: Error?) -> Void) {
+        let discoverService = DiscoverService()
+        var movies: [MovieData] = []
+
+        let formatter = DateFormatter.iso8601
+        let dateGTE = formatter.string(from: Date())
+        var dateLTE = ""
+
+        var dateComponent = DateComponents()
+        dateComponent.month = 1
+
+        if let lteDateData = Calendar.current.date(byAdding: dateComponent, to: Date()) {
+            dateLTE = formatter.string(from: lteDateData)
+        }
+
+        let parameters: [String: String] = [
+            "primary_release_date.gte": dateGTE,
+            "primary_release_date.lte": dateLTE,
+            "sort_by": "popularity.desc",
+            "region": "US",
+            "page": "\(self.currentPage)"
+        ]
+
+        discoverService.fetchDiscoverService(parameters: parameters, responseHandler: { (movieResult) in
+            self.maxPages = movieResult.totalPages
+            for movie in movieResult.results {
+                movies.append(MovieData(movie: movie))
             }
+
+            responseHandler(movies)
+        }) { (error) in
+            errorHandler(error)
         }
     }
 }
--- TheMovieDatabase/Networking/DiscoverService.swift
+++ TheMovieDatabase/Networking/DiscoverService.swift
@@ -12,30 +12,6 @@ class DiscoverService: GeneralService {
 
     let discoverURL = "discover/movie/"
 
-    func getUpcommingMovies(page: Int, responseHandler: @escaping (_ response: MovieResult) -> Void, errorHandler: @escaping (_ error: Error?) -> Void) {
-
-        let formatter = DateFormatter.iso8601
-        let dateGTE = formatter.string(from: Date())
-        var dateLTE = ""
-
-        var dateComponent = DateComponents()
-        dateComponent.month = 1
-
-        if let lteDateData = Calendar.current.date(byAdding: dateComponent, to: Date()) {
-            dateLTE = formatter.string(from: lteDateData)
-        }
-
-        let parameters: [String: String] = [
-            "primary_release_date.gte": dateGTE,
-            "primary_release_date.lte": dateLTE,
-            "sort_by": "popularity.desc",
-            "region": "US",
-            "page": "\(page)"
-        ]
-
-        self.fetchDiscoverService(parameters: parameters, responseHandler: responseHandler, errorHandler: errorHandler)
-    }
-
     func fetchDiscoverService(parameters: [String: String], responseHandler: @escaping (_ response: MovieResult) -> Void, errorHandler: @escaping (_ error: Error?) -> Void) {
         self.executeRequest(url: self.discoverURL, paramaters: parameters as [String : AnyObject], responseHandler: { (data) in
             do {
